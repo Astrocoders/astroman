64cc2a0d006f2e1064ca253086d26ef94b4cd022
eliabejr <eliabejunior@hotmail.com.br>
2018-10-10 08:22:15 -0300

    Update: add error handler

--- src/index.js
+++ src/index.js
@@ -9,4 +9,19 @@ const report = require(`./reporter`)
 
 global.Promise = require(`bluebird`)
 
+Promise.onPossiblyUnhandledRejection(error => {
+  report.error(error)
+  throw error
+})
+
+process.on(`unhandledRejection`, error => {
+  // This will exit the process in newer Node anyway so lets be consistent
+  // across versions and crash
+  report.panic(`UNHANDLED REJECTION`, error)
+})
+
+process.on(`uncaughtException`, error => {
+  report.panic(`UNHANDLED EXCEPTION`, error)
+})
+
 createCli(process.argv)
