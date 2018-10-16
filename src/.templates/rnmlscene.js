11860ebcf13cd357d8931b525e008814073e651c
eliabejr <eliabejunior@hotmail.com.br>
2018-10-16 13:09:13 -0300

    Fix: fs importing

--- src/.templates/rnmlscene.js
+++ src/.templates/rnmlscene.js
@@ -1,7 +1,15 @@
 const { stripIndent } = require('common-tags')
+const fs = require('fs')
 const chalk = require('chalk')
 
-module.exports.where = ({ name }) => `re/scenes/${name}.re`
+module.exports.where = (name) => {
+if (!fs.existsSync('scenes')){
+    fs.mkdirSync('scenes')
+    return `scenes/${name}.re`
+  } else {
+    return `scenes/${name}.re`
+  }
+}
 
 module.exports.args = { name: 'Scene name' }
 
@@ -13,7 +21,7 @@ module.exports.postBuild = () =>
     ),
   )
 
-module.exports.gen = ({ name }) => stripIndent`open Rebolt;
+module.exports.gen = (name) => stripIndent`open Rebolt;
 open NavigationConfig;
 let component = ReasonReact.statelessComponent("${name}");
 let make = (~navigation, _children) => {
