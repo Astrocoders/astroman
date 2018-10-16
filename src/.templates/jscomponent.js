541e26bd9400da601f56666b91c6e9b3a2b5fd09
eliabejr <eliabejunior@hotmail.com.br>
2018-10-16 12:45:58 -0300

    Update: add more generators

---
+++ src/.templates/jscomponent.js
@@ -0,0 +1,37 @@
+const { stripIndent } = require('common-tags')
+const chalk = require('chalk')
+const fs = require('fs')
+
+module.exports.args = { name: 'Component name' }
+
+module.exports.where = (name) => {
+  if (!fs.existsSync('components')){
+    fs.mkdirSync('components')
+    return `components/${name}.js`
+  } else {
+    return `components/${name}.js`
+  }
+}
+
+module.exports.postBuild = () =>
+  console.log('✅ ', chalk.green('Component successfully created'))
+
+module.exports.gen = (name) => stripIndent`
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ${name} = ({prop}) => (
+  <div data-testid="${name}Wrapper">{prop}</div>
+)
+
+${name}.defaultProps = {
+  prop: '',
+}
+
+${name}.propTypes = {
+  prop: PropTypes.string.isRequired,
+}
+
+export default ${name}
+`
