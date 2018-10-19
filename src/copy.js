129ad20c00861067c1ddac15d021a3ff87a261f8
eliabejr <eliabejunior@hotmail.com.br>
2018-10-19 13:21:10 -0300

    Update: get files from astrocoders repository on github

--- src/copy.js
+++ src/copy.js
@@ -1,30 +1,32 @@
 #!/usr/bin/env node
 
-const sysPath = require('path')
+const execa = require(`execa`)
 const fs = require('fs')
 const chalk = require('chalk')
 const report = require(`./reporter`)
 
-const copy = async (kind: string, name: string, destination: string) => {
-
-  const prevDir = sysPath.join(__dirname, '../..', destination)
-
-  report.info(`Copying ${kind} from ${destination}`)
-
-  fs.copyFile(`${prevDir}/src/${kind}/${name}`, `${name}`, (err) => {
-    if (err) throw err
-    console.log('✅ ', chalk.green(`${kind} ${name} successfully copied`))
-  });
-
+const spawn = (cmd: string) => {
+  const [file, ...args] = cmd.split(/\s+/)
+  return execa(file, args, { stdio: `inherit` })
 }
 
-module.exports = async (kind: string, name: string, destination: string) => {
+module.exports = async (kind: string, name: string, destination: string, branch: string) => {
   if (!name) {
     console.log(
       '🤷',
       chalk.red("Please, provide a name for your component."),
     )
   } else {
-    await copy(kind, name, destination)
+    report.info(`Copying ${kind} from ${destination} on branch ${branch}`)
+    process.chdir('src')
+
+    if (!fs.existsSync(`${kind}s`)){
+      fs.mkdirSync(`${kind}s`)
+      process.chdir(`${kind}s`)
+    } else {
+      process.chdir(`${kind}s`)
+    }
+    await spawn(`curl https://raw.githubusercontent.com/astrocoders/${destination}/${branch}/src/${kind}s/${name} --output ${name}`)
+    console.log('✅ ', chalk.green(`${kind} ${name} successfully copied!`))
   }
 }
